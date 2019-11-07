030b33f4c88e14348027d186d1e3931f76000325
Brian Lederman <brianldrmn@gmail.com>
2019-11-07 14:36:36 -0800

    Log in/out funcionality, began message framework

--- warbler-client/src/components/AuthForm.js
+++ warbler-client/src/components/AuthForm.js
@@ -20,8 +20,13 @@ export default class AuthForm extends Component {
     handleSubmit = e => {
         e.preventDefault();
         const authType = this.props.signUp ? "signup" : "signin";
-        this.props.onAuth(authType, this.state).then(() => {
-            console.log("You logged in successfully!");
+        this.props
+            .onAuth(authType, this.state)
+            .then(() => {
+                this.props.history.push("/");
+            })
+            .catch(() => {
+            return;
         });
     };
 
--- warbler-client/src/components/Homepage.js
+++ warbler-client/src/components/Homepage.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import currentUser from "../store/reducers/currentUser";
 
-const Homepage = () => (
-    <div className="home-hero">
-        <h1>What's Good?</h1>
-        <h4>New to the site?</h4>
-        <Link to="/signup" className="btn btn-primary">
-            Sign up here
-        </Link>
-    </div>
-);
+const Homepage = ({ currentUser }) => {
+    if (!currentUser.isAuthenticated) {
+        return(
+            <div className="home-hero">
+                <h1>What's Good?</h1>
+                <h4>New to the site?</h4>
+                <Link to="/signup" className="btn btn-primary">
+                    Sign up here
+                </Link>
+            </div>
+        );
+    }
+    return (
+        <div>
+            <h1>You are logged in!</h1>
+        </div>
+    );
+};
 
 export default Homepage;
